4cab2b4e297db466862d2d6c06e13b971e28d757
Etxebe <mikelgartzia21@gmail.com>
2024-02-13 08:50:31 +0100

    [NEW] creada funcion getSUperHeroes en gameService para obtener superHEroes

---
+++ gameService.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+async function getSuperheroes() {
+  try {
+    const response = await axios.get('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json');
+
+    console.log('JUGADORES', response.data);
+
+    return response.data;
+  } catch (error) {
+    console.error('Error al obtener los superhéroes:', error);
+    throw error; 
+  }
+}
+
+
+getSuperheroes();
+
